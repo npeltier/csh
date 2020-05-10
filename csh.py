e5df0c6d86bf25dc2a1ad50ac98b34d50791e19a
Nicolas Peltier <npeltier@adobe.com>
2020-05-10 23:54:03 +0200

    premier commit

---
+++ csh.py
@@ -0,0 +1,29 @@
+from random import randrange
+import matplotlib.pyplot as plt
+
+NB_TIRAGE = 10000
+
+def tirage():
+    return [randrange(6) + 1 for de in range(4)]
+
+def sommes(des):
+    _sommes = []
+    for de in range(4):
+        for autre in range(4):
+            if (de != autre):
+                _sommes.append(des[de] + des[autre])
+    return set(_sommes)
+
+def stats(dejaTire = 0):
+    stats = {2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10:0, 11:0, 12:0}
+    for jeter in range(NB_TIRAGE):
+        des = tirage()
+        _sommes = sommes(des)
+        if (dejaTire not in _sommes):
+            for resultat in _sommes:
+                stats[resultat] += 1
+    return stats
+
+data = stats(8)
+plt.bar(data.keys(), data.values())
+plt.show()
