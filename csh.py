13f9b8038d00de5994338a7397b7255d44a2253f
Nicolas Peltier <npeltier@adobe.com>
2020-05-11 21:52:37 +0200

    in sequence + README.md

--- csh.py
+++ csh.py
@@ -1,7 +1,7 @@
 from random import randrange
-import matplotlib.pyplot as plt
 
-NB_TIRAGE = 10000
+NB_TIRAGE = 20000
+WIDTH = 100
 
 def tirage():
     return [randrange(6) + 1 for de in range(4)]
@@ -24,6 +24,18 @@ def stats(dejaTire):
                 stats[resultat] += 1
     return stats
 
-data = stats({7,9})
-plt.bar(data.keys(), data.values())
-plt.show()
+def runAndPlot(dejaTire, overallMax):
+    print('\nProbabilité de tirage ayant pris les lignes', dejaTire)
+    data = stats(dejaTire)
+    ratio = overallMax / max(data.values())
+    for ligne in range(2,13):
+        gauge = "□" * int(data[ligne] * ratio * WIDTH / NB_TIRAGE)
+        espacement = " " * (1 - int(ligne / 10))
+        print (ligne, espacement, gauge)
+
+canonical = stats({})
+overallMax = canonical[7]
+for ligne1 in range(2,13):
+    for ligne2 in range (ligne1, 13):
+        if (ligne1 != ligne2):
+            runAndPlot({ligne1, ligne2}, overallMax)
