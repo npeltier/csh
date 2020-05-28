f0e9a088ce9e2633c4b037ab06e23f6dce677c17
Nicolas Peltier <npeltier@adobe.com>
2020-05-28 20:56:34 +0200

    ajout classement combinaison

--- csh.py
+++ csh.py
@@ -16,13 +16,38 @@ def sommes(des):
 
 def stats(dejaTire):
     stats = {2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10:0, 11:0, 12:0}
-    for jeter in range(NB_TIRAGE):
+    for jete in range(NB_TIRAGE):
         des = tirage()
         _sommes = sommes(des)
         if (not _sommes.intersection(dejaTire)):
             for resultat in _sommes:
                 stats[resultat] += 1
     return stats
+
+def tirageCount(tirages = {}):
+    tirageCount = {}     
+    for l1 in range(2,12):
+        for l2 in range(2,12):
+            for l3 in range(2,12):
+                if (l1 != l2) and (l1 != l3) and (l2 != l3):
+                    tirage = [l1, l2, l3]
+                    tirage.sort()
+                    tirageStr = ",".join(map(str,tirage))
+                    tirageCount[tirageStr] = 0
+                    tirages[tirageStr] = tirage
+    return tirageCount
+
+def classement(): 
+    tirages = {}
+    tiragesCount = tirageCount(tirages)
+    for jete in range(NB_TIRAGE):
+        des = tirage()
+        _sommes = sommes(des)
+        for somme in _sommes:
+            for tirageItem in tirages.items():
+                if somme in tirageItem[1]:
+                    tiragesCount[tirageItem[0]] += 1
+    return {k: v for k, v in sorted(tiragesCount.items(), key=lambda item: item[1])}
 
 def runAndPlot(dejaTire, overallMax):
     print('### Probabilité de tirage ayant pris les lignes', dejaTire, '\n')
@@ -34,9 +59,15 @@ def runAndPlot(dejaTire, overallMax):
         print (ligne, espacement, gauge, "\n")
 
 print('# Aide Can\'t stop\n')
+print('## Probabilités sachant 2 tirages\n')
+print("occurences sur ",NB_TIRAGE," d'une combinaison n'étant pas celles déjà tirées\n")
 canonical = stats({})
 overallMax = canonical[7]
 for ligne1 in range(2,13):
     for ligne2 in range (ligne1, 13):
         if (ligne1 != ligne2):
             runAndPlot({ligne1, ligne2}, overallMax)
+print('## Classement des combinaisons\n')
+print("occurences sur ",NB_TIRAGE," tirs des combinaisons non perdantes\n")
+for i in classement().items():
+    print(i[1], " fois ", i[0])
